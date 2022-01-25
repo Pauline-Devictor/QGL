d79e32bd88d52da1762c3a5655487b7aa2f44354
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-01-25 18:36:10 +0100

    Ajout Deck #5

--- player/src/main/java/fr/unice/polytech/si3/qgl/mugiwara_cook/ship/Deck.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/mugiwara_cook/ship/Deck.java
@@ -1,4 +1,33 @@
 package fr.unice.polytech.si3.qgl.mugiwara_cook.ship;
 
 public class Deck {
+    int width;
+    int length;
+
+    public Deck(){
+        //Json
+    }
+
+    public Deck(int width, int height){
+        this.width = width;
+        this.length = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+
 }
