56850d10684c7b274fdb0280b812345b0caa7eb7
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-01-28 17:27:12 +0100

    Ajout de Checkpoint #13

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/mugiwara_cook/Checkpoint.java
@@ -0,0 +1,35 @@
+package fr.unice.polytech.si3.qgl.mugiwara_cook;
+
+import fr.unice.polytech.si3.qgl.mugiwara_cook.shapes.Shape;
+
+public class Checkpoint {
+    Position position;
+    Shape shape;
+
+    public Checkpoint(){
+        //Json
+    }
+
+    public Checkpoint(Position position, Shape shape){
+        this.position = position;
+        this.shape = shape;
+    }
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+
+
+}
