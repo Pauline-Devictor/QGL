b6d215acb69ad3bd8ecc0a1a4e3d55c05909437b
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-02-03 14:55:36 +0100

    Stockage des données de début de tour et de partie #12

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/Cockpit.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/Cockpit.java
@@ -5,26 +5,40 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import fr.unice.polytech.si3.qgl.Mugiwara_Cook.game.InitGame;
+import fr.unice.polytech.si3.qgl.Mugiwara_Cook.game.NextRound;
 import fr.unice.polytech.si3.qgl.regatta.cockpit.ICockpit;
 
 public class Cockpit implements ICockpit {
-
+	MyMapper myMapper= new MyMapper();
 	public Cockpit(){
 		//Json
 	}
 
+	/**
+	 *Init the game datas
+	 * @param game A Json with the beginning data
+	 */
 	public void initGame(String game) {
 		System.out.println("Init game input: " + game);
-		MyMapper myMapper= new MyMapper();
 		try {
-			myMapper.readValue(game, InitGame.class);
+			InitGame initGame = myMapper.readValue(game, InitGame.class);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 *Decide what to do with round information
+	 * @param round Json with data for the next round
+	 * @return Json with the list of our actions
+	 */
 	public String nextRound(String round) {
 		System.out.println("Next round input: " + round);
+		try {
+			NextRound nextRound = myMapper.readValue(round, NextRound.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 		return "[ {\n" +
 				"    \"sailorId\": 0,\n" +
 				"    \"type\": \"OAR\"\n" +
