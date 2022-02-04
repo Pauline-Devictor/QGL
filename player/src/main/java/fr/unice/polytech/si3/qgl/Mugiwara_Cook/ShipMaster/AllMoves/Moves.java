f746add5b1acaae7bcecdd413bf111cbfb9b9342
EmileDerain <emilederain@gmail.com>
2022-02-04 18:17:12 +0100

    Creation class #16

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ShipMaster/AllMoves/Moves.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.si3.qgl.Mugiwara_Cook.ShipMaster.AllMoves;
+
+abstract public class Moves {
+    int x;
+    int y;
+    int orientation;
+}
---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ShipMaster/MoveMark.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.si3.qgl.Mugiwara_Cook.ShipMaster;
+
+public class MoveMark {
+}
