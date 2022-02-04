eb88feb6dfbc68adc1987992e1f951dca28de91c
EmileDerain <emilederain@gmail.com>
2022-02-04 18:24:35 +0100

    Add class #16

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ShipMaster/AllMoves/Moves.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ShipMaster/AllMoves/Moves.java
@@ -3,5 +3,11 @@ package fr.unice.polytech.si3.qgl.Mugiwara_Cook.ShipMaster.AllMoves;
 abstract public class Moves {
     int x;
     int y;
-    int orientation;
+    double orientation;
+
+    public Moves() {
+        x = 1;
+        y = 1;
+        orientation = 3.14;
+    }
 }
