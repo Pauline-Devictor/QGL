79db02b309101b5508fb22a37369c990ee014716
habib belmokhtar <habib.belmokhtar@etu.unice.fr>
2022-02-05 12:19:44 +0100

    verification du nombe de deplacement+ test associé #15

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/actions/Moving.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/actions/Moving.java
@@ -1,5 +1,9 @@
 package fr.unice.polytech.si3.qgl.Mugiwara_Cook.actions;
 
+import java.lang.Math;
+
+import static java.lang.Math.abs;
+
 public class Moving extends Action{ //TODO condition xdistance and ydistance
     public final static String TYPE = "Moving";
     int sailorId;
@@ -12,6 +16,22 @@ public class Moving extends Action{ //TODO condition xdistance and ydistance
         this.xdistance = xdistance;
         this.ydistance = ydistance;
     }
+
+    /**
+     * @param xdistance le nombre de déplacement en x
+     * @param ydistance le nombre de déplacement en y
+     * @return un boolean qui dit vrai s'il respect les conditions, faux sinon
+     */
+    public boolean isAllowedToMove(int xdistance, int ydistance){
+        if(abs(xdistance)+abs(ydistance)<=5) return true;
+        return false;
+    }
+
+    //public boolean isNotGoingToFallInTheSea(int sailorId, int xdistance, int ydistance){
+
+
+
+
 
     public int getSailorId() {
         return sailorId;
