9b7b9945a548d491649e49680be4204e3382e254
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-02-10 11:41:01 +0100

    Amelioration des equipements #24

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ship/equipment/Canon.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ship/equipment/Canon.java
@@ -1,16 +1,18 @@
 package fr.unice.polytech.si3.qgl.Mugiwara_Cook.ship.equipment;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Canon extends Equipment {
     public final static String TYPE = "canon";
     int x;
     int y;
     boolean loaded;
     double angle;
+    @JsonIgnore
+    boolean used = false;
 
     public Canon(int x, int y,boolean loaded,double angle) {
-        super(TYPE);
-        this.x = x;
-        this.y = y;
+        super(TYPE,x,y);
         this.loaded = loaded;
         this.angle = angle;
     }
@@ -47,4 +49,11 @@ public class Canon extends Equipment {
         this.angle = angle;
     }
 
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
 }
--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ship/equipment/Oar.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/ship/equipment/Oar.java
@@ -1,5 +1,7 @@
 package fr.unice.polytech.si3.qgl.Mugiwara_Cook.ship.equipment;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * Rame
  */
@@ -7,12 +9,12 @@ public class Oar extends Equipment {
     public final static String TYPE = "oar";
     int x;
     int y;
+    @JsonIgnore
+    boolean used = false;
 
 
     public Oar(int x,int y) {
-        super(TYPE);
-        this.x = x;
-        this.y = y;
+        super(TYPE,x,y);
     }
 
     public int getX() {
@@ -31,4 +33,11 @@ public class Oar extends Equipment {
         this.y = y;
     }
 
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
 }
