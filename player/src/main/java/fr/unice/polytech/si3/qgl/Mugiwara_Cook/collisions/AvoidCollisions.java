90d748ed06122468dabfefca8ed0d119900de114
nddav <alexandre.nunes-dias@etu.univ-cotedazur.fr>
2022-03-28 16:17:38 +0200

    Ajout de la stratégie d'évitement des reefs #32

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/collisions/AvoidCollisions.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/collisions/AvoidCollisions.java
@@ -25,12 +25,11 @@ public class AvoidCollisions {
         return currentCheckpoint;
     }
 
-    public void setCurrentCheckpoint(Checkpoint newCurrentCheckpoint) { this.currentCheckpoint = currentCheckpoint; }
+    public void setCurrentCheckpoint(Checkpoint newCurrentCheckpoint) { this.currentCheckpoint = newCurrentCheckpoint; }
 
     /**
      * Cette méthode regarde si un obstacle se situe à droite ou à gauche de la droite formée par le bateau et le checkpoint
-     * @param obstaclePoint
-     * @return
+     * @return un booléan qui est vrai si le point est à droite
      */
     public boolean isPointOnTheRightOfShip(Point obstaclePoint){
         double xVectorShipToCheckpoint = currentCheckpoint.getPosition().getX() - ship.getPosition().getX();
@@ -39,27 +38,41 @@ public class AvoidCollisions {
         double yVectorShipToObstaclepoint = obstaclePoint.getY() - ship.getPosition().getY();
 
         double d = xVectorShipToCheckpoint*yVectorShipToObstaclepoint - yVectorShipToCheckpoint*xVectorShipToObstaclepoint;
-        if(d>0) { return true; }
-        else { return false; }
+        return d > 0;
+    }
+
+    // A changer si la forme du bateau change !!!!
+    // Marche pour les cercles
+    public Position calculateNewPosition(Point start, Point finish){
+
+        //double distanceCenterToNewPosition = ((Circle)reef.getShape()).getRadius() + ((Rectangle)ship.getShape()).getHeight()*2;
+        double yReefCenter = reef.getPosition().getY();
+        double xReefCenter = reef.getPosition().getX();
+        double distanceCenterToNewPosition = determineDistance(xReefCenter,yReefCenter);
+        double xVectorShipToCheckpoint = currentCheckpoint.getPosition().getX() - ship.getPosition().getX();
+        double yVectorShipToCheckpoint = currentCheckpoint.getPosition().getY() - ship.getPosition().getY();
+        /**
+         * A SUPPRIMER UNE FOIS QUE TOUT MARCHE
+        double aCoefficient = 1 + (xVectorShipToCheckpoint/yVectorShipToCheckpoint);
+        double bCoefficient = -2*yReefCenter*(1 + Math.pow(xVectorShipToCheckpoint/yVectorShipToCheckpoint,2));
+        double cCoefficient = Math.pow(yReefCenter,2) + Math.pow(xReefCenter,2) + Math.pow(xReefCenter - (xVectorShipToCheckpoint/yVectorShipToCheckpoint)*yReefCenter,2) - 2*xReefCenter*(xReefCenter-yReefCenter*(xVectorShipToCheckpoint/yVectorShipToCheckpoint)) - Math.pow(distanceCenterToNewPoint,2);
+        double delta = Math.pow(bCoefficient,2) - 4*aCoefficient*cCoefficient;
+
+        double ySolution1 = (-bCoefficient + Math.sqrt(delta))/2*aCoefficient;
+        double xSolution1 = xReefCenter + (ySolution1 - yReefCenter)*xVectorShipToCheckpoint/yVectorShipToCheckpoint;
+         */
+        Position potentialPosition = resolveEquation(xVectorShipToCheckpoint,yVectorShipToCheckpoint,xReefCenter,yReefCenter,distanceCenterToNewPosition);
+
+        return determineRightPosition(potentialPosition.getX(),potentialPosition.getY(),xReefCenter,yReefCenter);
+
     }
 
     /**
-    public Checkpoint avoidCircleReef(){
-        Position checkpointPosition;
-        return;
-    }
+     * Cette méthode résout le système à deux inconnues pour déduire les coordonées d'un nouveau checkpoint
+     * @return une position qui est potentiellement du bon côté du checkpoint
      */
+    public Position resolveEquation(double xVectorShipToCheckpoint,double yVectorShipToCheckpoint, double xReefCenter, double yReefCenter,double distanceCenterToNewPoint ){
 
-    // A changer si la forme du bateau change !!!!
-    public Position calculateNewPoint(Point start, Point finish){
-
-        double distanceCenterToNewPoint = ((Circle)reef.getShape()).getRadius() + ((Rectangle)ship.getShape()).getHeight()*2;
-        double yReefCenter = reef.getPosition().getY();
-        double xReefCenter = reef.getPosition().getX();
-        double xVectorShipToCheckpoint = currentCheckpoint.getPosition().getX() - ship.getPosition().getX();
-        double yVectorShipToCheckpoint = currentCheckpoint.getPosition().getY() - ship.getPosition().getY();
-
-        /// methode resolve equation ?
         double aCoefficient = 1 + (xVectorShipToCheckpoint/yVectorShipToCheckpoint);
         double bCoefficient = -2*yReefCenter*(1 + Math.pow(xVectorShipToCheckpoint/yVectorShipToCheckpoint,2));
         double cCoefficient = Math.pow(yReefCenter,2) + Math.pow(xReefCenter,2) + Math.pow(xReefCenter - (xVectorShipToCheckpoint/yVectorShipToCheckpoint)*yReefCenter,2) - 2*xReefCenter*(xReefCenter-yReefCenter*(xVectorShipToCheckpoint/yVectorShipToCheckpoint)) - Math.pow(distanceCenterToNewPoint,2);
@@ -68,39 +81,50 @@ public class AvoidCollisions {
         double ySolution1 = (-bCoefficient + Math.sqrt(delta))/2*aCoefficient;
         double xSolution1 = xReefCenter + (ySolution1 - yReefCenter)*xVectorShipToCheckpoint/yVectorShipToCheckpoint;
         //double ySolution2 = (-bCoefficient - Math.sqrt(delta))/2*aCoefficient;
-
-        return determineRightPosition(ySolution1,xSolution1,xReefCenter,yReefCenter);
-
+        return new Position(xSolution1,ySolution1,0.0);
     }
 
+    /**
+     * Cette méthode choisit une position entre celle de la solution et son symétrique
+     * @return la bonne position
+     */
     public Position determineRightPosition(double ySolution1,double xSolution1,double xreefCenter,double yReefCenter){
 
         Point potientialPoint = new Point(xSolution1,ySolution1);
         Point reefCenter = new Point(xreefCenter,yReefCenter);
         if(isPointOnTheRightOfShip(reefCenter)) {
-            if(isPointOnTheRightOfShip(reefCenter)) { return new Position(xSolution1,ySolution1,0.0); }
-            else {  return calculateOppositePosition(reefCenter,potientialPoint); }
+            if(isPointOnTheRightOfShip(potientialPoint)) { return new Position(xSolution1,ySolution1,0.0); }
+            else {  return calculateSymetricalPosition(reefCenter,potientialPoint); }
         } else {
-            if((!isPointOnTheRightOfShip(reefCenter))) { return new Position(xSolution1,ySolution1,0.0); }
-            else { return calculateOppositePosition(reefCenter,potientialPoint); }
+            if((!isPointOnTheRightOfShip(potientialPoint))) { return new Position(xSolution1,ySolution1,0.0); }
+            else { return calculateSymetricalPosition(reefCenter,potientialPoint); }
         }
     }
 
     /**
-     * @param reefCenter
      * @param potentialCheckPointCenter position dont on veut le symétrique par rapport au position reefCenter
      */
-    public Position calculateOppositePosition(Point reefCenter, Point potentialCheckPointCenter) {
+    public Position calculateSymetricalPosition(Point reefCenter, Point potentialCheckPointCenter) {
         double xNewPosition = 2*reefCenter.getX() - potentialCheckPointCenter.getX();
         double yNewPosition = 2*reefCenter.getY() - potentialCheckPointCenter.getY();
         return new Position(xNewPosition,yNewPosition,0.0);
     }
 
-    // determineRightPoint verifie en fonction de si c'est a droite ou pas la bonne solution
-
-    /**
-    public Checkpoint avoidPolygon(){
-        return
+    public double determineDistance(double xReefCenter, double yReefCenter){
+        if(reef.getShape().equals("circle")){ return ((Circle)reef.getShape()).getRadius() + ((Rectangle)ship.getShape()).getHeight()*2; }
+        else if (reef.getShape().equals("rectangle")) { return ((Rectangle)reef.getShape()).getHeight()*(3/2) + ((Rectangle)ship.getShape()).getHeight(); }
+        else { return findFurthestPointFromPolygon(xReefCenter, yReefCenter); }
     }
-     */
+
+    public double findFurthestPointFromPolygon(double xReefCenter, double yReefCenter){
+        Point furthestPoint = ((Polygon)reef.getShape()).getVertices()[0];
+        Point reefCenter = new Point(xReefCenter,yReefCenter);
+        boolean rightOfShip = isPointOnTheRightOfShip(reefCenter);
+        for( Point vertices : ((Polygon)reef.getShape()).getVertices() ){
+            if(rightOfShip!=isPointOnTheRightOfShip(vertices) && Math.sqrt(Math.pow(xReefCenter - vertices.getX(),2) + Math.pow(yReefCenter- vertices.getY(),2))>= Math.sqrt(Math.pow(xReefCenter - furthestPoint.getX(),2) + Math.pow(yReefCenter- furthestPoint.getY(),2))){
+                furthestPoint=vertices;
+            }
+        }
+        return Math.sqrt(Math.pow(xReefCenter - furthestPoint.getX(),2) + Math.pow(yReefCenter- furthestPoint.getY(),2));
+    }
 }
