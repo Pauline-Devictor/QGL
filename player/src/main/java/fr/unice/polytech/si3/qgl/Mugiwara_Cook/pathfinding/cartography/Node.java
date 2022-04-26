8f5e948aa7d8dfe4dac21c6c979913835a10e367
habib belmokhtar <habib.belmokhtar@etu.unice.fr>
2022-04-26 16:19:44 +0200

    refactor du path/collision en commentaire pour l'instant afin d'avoir l'avis des personnes l'ayant fait #52

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/pathfinding/cartography/Node.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/pathfinding/cartography/Node.java
@@ -78,6 +78,28 @@ public class Node {
 
     }
 
+
+    /**public void addVoisin(List<List<Node>> grid) {
+        int cols = grid.get(0).size();
+        int rows = grid.size();
+        aRenameNode1(this.x < cols - 1, grid, this.y, this.x > 0, this.y, this.y < rows - 1, this.x, this.y > 0, this.x);
+
+
+        aRenameNode1(this.x < cols - 1 && this.y < rows - 1, grid, this.y + 1, this.x > 0 && this.y > 0, this.y - 1, this.y < rows - 1 && this.x > 0, this.x - 1, this.y > 0 && this.x < cols - 1, this.x + 1);
+
+    }
+
+    public void aRenameNode1(boolean x, List<List<Node>> grid, int y, boolean x1, int y1, boolean y2, int x2, boolean y3, int x3) {
+        if (x)
+            this.voisin.add(grid.get(y).get(this.x + 1));
+        if (x1)
+            this.voisin.add(grid.get(y1).get(this.x - 1));
+        if (y2)
+            this.voisin.add(grid.get(this.y + 1).get(x2));
+        if (y3)
+            this.voisin.add(grid.get(this.y - 1).get(x3));
+    }**/
+
     public void setWall(boolean wall) {
         this.wall = wall;
         if (this.wall) {
