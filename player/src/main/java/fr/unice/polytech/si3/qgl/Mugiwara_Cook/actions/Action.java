c32862ce701a39e6c2faf55a5a95b383c82f734c
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-05-20 14:53:49 +0200

    Legère correction #56

--- player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/actions/Action.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/Mugiwara_Cook/actions/Action.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 public abstract class Action {
+
+    @Getter
+    String type;
     @Getter
     @Setter
     int sailorId;
-    @Getter
-    String type;
-
     protected Action(String type, int sailorId) {
         this.type = type;
         this.sailorId = sailorId;
